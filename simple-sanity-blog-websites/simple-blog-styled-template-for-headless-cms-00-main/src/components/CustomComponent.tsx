aa6a0346448b32c8e3f95c3b3e226e2e4edf43c2
ismat <ismatfatima213@gmail.com>
2025-01-02 20:33:16 +0500

    Re-added all required files

---
+++ simple-sanity-blog-websites/simple-blog-styled-template-for-headless-cms-00-main/src/components/CustomComponent.tsx
@@ -0,0 +1,15 @@
+import {PortableTextComponents} from '@portabletext/react';
+
+export const components: PortableTextComponents = {
+     block:{
+          h3:({children})=><h3 className='text-3xl font-bold text-accentDarkPrimary'>
+          {children}</h3>
+     },
+     listItem:{
+          bullet:({children})=><li className='list-disc marker:text-accentDarkSecondary list-inside ml-4'>{children}</li>
+     },
+
+     marks:{
+          strong:({children})=><strong className='font-bold text-white dark:text-dark'>{children}</strong>
+     }
+}
